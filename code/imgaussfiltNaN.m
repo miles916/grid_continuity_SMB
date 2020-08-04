1479223f485856cd7da2006cbae59ce50fb41ba7
miles916 <evan.miles@wsl.ch>
2020-08-04 12:47:24 +0200

    add imgaussfiltNaN
    -add function to gaussian-filter ignoring NaNs
    -commenting for zonal_aggregate_v2

---
+++ code/imgaussfiltNaN.m
@@ -0,0 +1,64 @@
+function Z = imgaussfiltNaN(im,sig,varargin)
+%imgaussfiltNaN - Function to implement a gaussian filter as in imgaussfilt() but ignoring NaN values. follows approach at:
+%https://stackoverflow.com/questions/18697532/gaussian-filtering-a-image-with-nan-in-python
+
+%
+% Syntax:  Z = imgaussfiltNaN(im,sig)
+%          Z = imgaussfiltNaN(im,sig,erasenans)
+%          Z = imgaussfiltNaN(______,Name,Value)
+%
+% Inputs:
+%    im - image to be gaussian-filtered, containing NaNs
+%    sig - standard deviation for gaussian kernel
+%    erasenans - (optional) flag to use nan values in final output (=1, default), or to provide gap-filled filtered results (=0)
+%    Name,Value - Name-Value pairs used to control aspects of the filtering as per imgaussfilt. Not tested thoroughly 
+%
+% Outputs:
+%    Z - filtered image
+%
+%
+% Author: Evan Miles
+% Work address: Swiss Federal Research Institute WSL
+% Email: evan.miles@wsl.ch
+% Sep 2019; Last revision: 03-Aug-2020
+
+%% ------------- BEGIN CODE --------------
+%parse inputs
+nvarin=numel(varargin);
+if nvarin>0
+    if mod(nvarin,2)==1 %odd number of variable inputs - contains erasenans
+        erasenans=varargin{1};
+        if nvarin>1
+            opts=[varargin{2:end}];
+        end
+    else % even number of inputs - erasenans not specified
+        erasenans=1;
+    end
+else
+    erasenans=1;
+end
+
+
+
+%% set up U,V inputs
+nans=isnan(im); %find nans
+V=im; %duplicate, replace nanas with 0
+V(nans)=0;
+
+W=1-nans; %logical array where non-nan
+
+%% filter both
+if exist('opts')
+    VV=imgaussfilt(V,sig,opts);
+    WW=imgaussfilt(W,sig,opts);
+else
+    VV=imgaussfilt(V,sig);
+    WW=imgaussfilt(W,sig);
+end
+
+%ratio is filtered data ignoring NaNs
+Z=VV./WW;
+
+if erasenans==1 %option to remove NaNs again or not
+    Z(nans)=NaN;
+end
--- code/zonal_aggregate_v2.m
+++ code/zonal_aggregate_v2.m
@@ -1,14 +1,33 @@
 function [zVAR] = zonal_aggregate_v2(zones,VAR,method)
+%zonal_aggregate_v2 - Function to loop through segmentation and aggregate a
+%variable to a mean value ignoring NaNs
+%
+% Syntax:  [zVAR] = zonal_aggregate(zones,VAR)
+%
+% Inputs:
+%    zones - segmentation (uint16 raster) of domains
+%    VAR - raster of variable to be aggregated to each zone
+%    method - can be 'mean' for nan-ignoring mean zonal value,'rms' for root-mean-squared (does not ignore nans) ,or 'rssn' for root-sum-squared normalized by the count (ignores nans)
+%
+% Outputs:
+%    zVAR - mean value of VAR for each zone
+%
+%
+% Author: Evan Miles
+% Work address: Swiss Federal Research Institute WSL
+% Email: evan.miles@wsl.ch
+% Sep 2019; Last revision: 03-Aug-2020
 
-mask = isnan(zones);
+%% ------------- BEGIN CODE --------------
+mask = isnan(zones); %find overall domain
 
-zVAR = zeros(size(VAR));
+zVAR = zeros(size(VAR));  %initialize output
 
-zonelist = unique(zones(:));
+zonelist = unique(zones(:)); %list of zones
 
 switch method
     case 'mean'
-        for iz = 1:length(zonelist)
+        for iz = 1:length(zonelist) %loop through zones and derive nanmean
             cz = zonelist(iz);
             curzone = (zones==cz);
             cval = nanmean(VAR(curzone(:)==1));
